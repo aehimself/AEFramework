6559a4312159ea01c0c8b9a277d377b3111f4189
AEHimself <57598335+aehimself@users.noreply.github.com>
2022-12-07 23:59:04 +0100

    All DDE calls should be now thread-safe. Delphi instances expose window captions. All objects in DelphiVersion are now derived from TComponent. Added company name to versions. TDelphiVersions now have a .LatestVersion property

--- AE.DelphiVersions.pas
+++ AE.DelphiVersions.pas
@@ -2,26 +2,28 @@
 
 Interface
 
-Uses System.Generics.Collections, WinApi.Windows, System.SysUtils;
+Uses System.Generics.Collections, WinApi.Windows, System.SysUtils, System.Classes;
 
 Type
-  TDelphiInstance = Class
+  TDelphiInstance = Class(TComponent)
   strict private
     _ddehwnd: HWND;
     _idehwnd: HWND;
+    _idecaption: String;
     _pid: Cardinal;
     Function GlobalLockString(inString: string; inFlags: UINT): THandle;
   public
-    Constructor Create(Const inPID: Cardinal; Const inDDEHWND: HWND);
-    Procedure FindIdeWindow;
+    Constructor Create(inOwner: TComponent; Const inPID: Cardinal; Const inDDEHWND: HWND); ReIntroduce;
     Procedure OpenFile(Const inFileName: String);
+    Function FindIdeWindow: Boolean;
     Function IsIDEBusy: Boolean;
+    Property IDECaption: String Read _idecaption;
     Property IDEHWND: HWND Read _idehwnd;
     Property PID: Cardinal Read _pid;
     Property DDEHWND: HWND Read _ddehwnd;
   End;
 
-  TBorlandDelphiVersion = Class
+  TBorlandDelphiVersion = Class(TComponent)
   strict private
     _bdspath: String;
     _instances: TObjectList<TDelphiInstance>;
@@ -33,7 +35,7 @@ Type
     Function GetDelphiName: String; Virtual;
   public
     Class Function BDSRoot: String; Virtual;
-    Constructor Create(Const inBDSPath: String; Const inVersionNumber: Byte); ReIntroduce;
+    Constructor Create(inOwner: TComponent; Const inBDSPath: String; Const inVersionNumber: Byte); ReIntroduce;
     Destructor Destroy; Override;
     Procedure RefreshInstances;
     Function IsRunning: Boolean;
@@ -66,28 +68,31 @@ Type
     Class Function BDSRoot: String; Override;
   End;
 
-  TDelphiVersions = Class
+  TDelphiVersions = Class(TComponent)
   strict private
+    _latestversion: TBorlandDelphiVersion;
     _versions: TObjectList<TBorlandDelphiVersion>;
     Function GetInstalledVersions: TArray<TBorlandDelphiVersion>;
   public
-    Constructor Create; ReIntroduce;
+    Constructor Create(inOwner: TComponent); Override;
     Destructor Destroy; Override;
     Function ByName(Const inName: String): TBorlandDelphiVersion;
     Function ByVersionNumber(Const inVersionNumber: Integer): TBorlandDelphiVersion;
     Property InstalledVersions: TArray<TBorlandDelphiVersion> Read GetInstalledVersions;
+    Property LatestVersion: TBorlandDelphiVersion Read _latestversion;
   End;
 
   EDelphiVersionException = Class(Exception);
 
 Implementation
 
-Uses System.Win.Registry, System.Classes, Vcl.DdeMan, WinApi.DDEml, WinApi.PsAPI, WinAPi.Messages;
+Uses System.Win.Registry, WinApi.DDEml, WinApi.PsAPI, WinAPi.Messages;
 
 Type
   TWindowInfo = Record
-    ProcessID: Cardinal;
-    HWND: HWND;
+    outHWND: HWND;
+    outWindowCaption: String;
+    PID: Cardinal;
   End;
   PWindowInfo = ^TWindowInfo;
 
@@ -95,8 +100,7 @@ Const
   DDESERVICE = 'bds';
   DDETOPIC = 'system';
 
-
-Function EnumWindowsProc(inHWND: HWND; inParam: LParam): Boolean; StdCall;
+Function FindDelphiWindow(inHWND: HWND; inParam: LParam): Boolean; StdCall;
 Var
   ppid: Cardinal;
   title, classname: Array[0..255] Of Char;
@@ -109,20 +113,30 @@ Begin
   GetWindowText(inHWND, title, 255);
   GetClassName(inHWND, classname, 255);
 
-  Result := (ppid <> PWindowInfo(inParam)^.ProcessID) Or Not IsWindowVisible(inHWND) Or (Not IsWindowEnabled(inHWND)) Or Not (String(title).Contains('RAD Studio') Or String(title).Contains('Delphi')) Or (String(classname) <> 'TAppBuilder');
+  Result := (ppid <> PWindowInfo(inParam)^.PID) Or Not IsWindowVisible(inHWND) Or (Not IsWindowEnabled(inHWND)) Or Not (String(title).Contains('RAD Studio') Or String(title).Contains('Delphi')) Or (String(classname) <> 'TAppBuilder');
   If Not Result Then
-    PWindowInfo(inParam)^.HWND := inHWND;
+  Begin
+    PWindowInfo(inParam)^.outHWND := inHWND;
+    PWindowInfo(inParam)^.outWindowCaption := title;
+  End;
+End;
+
+Function DdeMgrCallBack(CallType, Fmt: UINT; Conv: HConv; hsz1, hsz2: HSZ; Data: HDDEData; Data1, Data2: ULONG_PTR): HDDEData; StdCall;
+Begin
+  Result := 0;
 End;
 
 //
 // TDelphiInstance
 //
 
-Constructor TDelphiInstance.Create(Const inPID: Cardinal; Const inDDEHWND: HWND);
+Constructor TDelphiInstance.Create(inOwner: TComponent; Const inPID: Cardinal; Const inDDEHWND: HWND);
 Begin
- inherited Create;
+ inherited Create(inOwner);
 
  _ddehwnd := inDDEHWND;
+ _idehwnd := 0;
+ _idecaption := '';
  _pid := inPID;
 
  FindIdeWindow;
@@ -159,21 +173,28 @@ Begin
   End;
 End;
 
-Procedure TDelphiInstance.FindIdeWindow;
+Function TDelphiInstance.FindIdeWindow: Boolean;
 Var
   info: PWindowInfo;
 Begin
+  _idehwnd := 0;
+  _idecaption := '';
+
   New(info);
   Try
-   info^.ProcessID := _pid;
-   info^.HWND := 0;
+   info^.PID := _pid;
+   info^.outHWND := 0;
+   info^.outWindowCaption := '';
 
-   EnumWindows(@EnumWindowsProc, LParam(info));
+   EnumWindows(@FindDelphiWindow, LParam(info));
 
-   _idehwnd := info^.HWND;
+   _idehwnd := info^.outHWND;
+   _idecaption := info^.outWindowCaption;
   Finally
    Dispose(info);
   End;
+
+  Result := _idehwnd <> 0;
 End;
 
 Function TDelphiInstance.GlobalLockString(inString: String; inFlags: UINT): THandle;
@@ -220,9 +241,9 @@ Begin
   Result := 'SOFTWARE\Borland\Delphi'
 End;
 
-Constructor TBorlandDelphiVersion.Create(Const inBDSPath: String; Const inVersionNumber: Byte);
+Constructor TBorlandDelphiVersion.Create(inOwner: TComponent; Const inBDSPath: String; Const inVersionNumber: Byte);
 Begin
-  inherited Create;
+  inherited Create(inOwner);
 
   _bdspath := inBDSPath;
   _instances := TObjectList<TDelphiInstance>.Create(True);
@@ -231,6 +252,8 @@ Begin
   _name := GetDelphiName;
   If _name.IsEmpty Then
     _name := 'BDS ' + _versionnumber.ToString + '.0';
+
+  Self.RefreshInstances;
 End;
 
 Function TBorlandDelphiVersion.IsRunning: Boolean;
@@ -265,36 +288,46 @@ Var
   conv: HConv;
   convinfo: TConvInfo;
   pid: Cardinal;
+  ddeid: Integer;
+  res: NativeUInt;
 Begin
   // DDE logic by Attila Kovacs
   // https://en.delphipraxis.net/topic/7955-how-to-open-a-file-in-the-already-running-ide/?do=findComment&comment=66850
 
   _instances.Clear;
 
-  apphandle := DdeCreateStringHandleW(ddeMgr.DdeInstId, PChar(DDESERVICE), CP_WINUNICODE);
-  topichandle := DdeCreateStringHandleW(ddeMgr.DdeInstId, PChar(DDETOPIC), CP_WINUNICODE);
+  ddeid := 0;
+  res := DdeInitializeW(ddeid, DdeMgrCallBack, APPCLASS_STANDARD, 0);
+  If res <> 0 Then
+    Raise EDelphiVersionException.Create('DDE error ' + DdeGetLastError(res).ToString);
   Try
-    convlist := DdeConnectList(ddeMgr.DdeInstId, apphandle, topichandle, 0, nil);
+    apphandle := DdeCreateStringHandleW(ddeid, PChar(DDESERVICE), CP_WINUNICODE);
+    topichandle := DdeCreateStringHandleW(ddeid, PChar(DDETOPIC), CP_WINUNICODE);
     Try
-      conv := 0;
-      Repeat
-        conv := DdeQueryNextServer(convlist, conv);
-        If conv = 0 Then
-          Break;
+      convlist := DdeConnectList(ddeid, apphandle, topichandle, 0, nil);
+      Try
+        conv := 0;
+        Repeat
+          conv := DdeQueryNextServer(convlist, conv);
+          If conv = 0 Then
+            Break;
 
-        convinfo.cb := SizeOf(TConvInfo);
-        DdeQueryConvInfo(conv, QID_SYNC, @convinfo);
+          convinfo.cb := SizeOf(TConvInfo);
+          DdeQueryConvInfo(conv, QID_SYNC, @convinfo);
 
-        GetWindowThreadProcessId(convinfo.hwndPartner, pid);
-        If ProcessName(pid).ToLower = _bdspath.ToLower Then
-          _instances.Add(TDelphiInstance.Create(pid, convinfo.hwndPartner));
-      Until (conv = 0);
+          GetWindowThreadProcessId(convinfo.hwndPartner, pid);
+          If ProcessName(pid).ToLower = _bdspath.ToLower Then
+            _instances.Add(TDelphiInstance.Create(Self, pid, convinfo.hwndPartner));
+        Until (conv = 0);
+      Finally
+       DdeDisconnectList(convlist);
+      End;
     Finally
-     DdeDisconnectList(convlist);
+      DdeFreeStringHandle(ddeid, apphandle);
+      DdeFreeStringHandle(ddeid, topichandle);
     End;
   Finally
-    DdeFreeStringHandle(ddeMgr.DdeInstId, apphandle);
-    DdeFreeStringHandle(ddeMgr.DdeInstId, topichandle);
+    DdeUninitialize(ddeid);
   End;
 End;
 
@@ -309,9 +342,9 @@ Function TBorlandDelphiVersion.GetDelphiName: String;
 Begin
   Case Self.VersionNumber Of
     6:
-      Result := 'Delphi 6';
+      Result := 'Borland Delphi 6';
     7:
-      Result := 'Delphi 7';
+      Result := 'Borland Delphi 7';
     Else
       Result := '';
   End;
@@ -335,11 +368,11 @@ Function TBorland2DelphiVersion.GetDelphiName: String;
 Begin
   Case Self.VersionNumber Of
     3:
-      Result := 'Delphi 2005';
+      Result := 'Borland Delphi 2005';
     4:
-      Result := 'Delphi 2006';
+      Result := 'Borland Delphi 2006';
     5:
-      Result := 'Delphi 2007';
+      Result := 'Borland Delphi 2007';
     Else
       Result := '';
   End;
@@ -358,9 +391,9 @@ Function TCodegearDelphiVersion.GetDelphiName: String;
 Begin
   Case Self.VersionNumber Of
     6:
-      Result := 'Delphi 2009';
+      Result := 'CodeGear Delphi 2009';
     7:
-      Result := 'Delphi 2010';
+      Result := 'CodeGear Delphi 2010';
     Else
       Result := '';
   End;
@@ -379,33 +412,33 @@ Function TEmbarcaderoDelphiVersion.GetDelphiName: String;
 Begin
   Case Self.VersionNumber Of
     8:
-      Result := 'Delphi XE';
+      Result := 'Embarcadero Delphi XE';
     9:
-      Result := 'Delphi XE2';
+      Result := 'Embarcadero Delphi XE2';
     10:
-      Result := 'Delphi XE3';
+      Result := 'Embarcadero Delphi XE3';
     11:
-      Result := 'Delphi XE4';
+      Result := 'Embarcadero Delphi XE4';
     12:
-      Result := 'Delphi XE5';
+      Result := 'Embarcadero Delphi XE5';
     14:
-      Result := 'Delphi XE6';
+      Result := 'Embarcadero Delphi XE6';
     15:
-      Result := 'Delphi XE7';
+      Result := 'Embarcadero Delphi XE7';
     16:
-      Result := 'Delphi XE8';
+      Result := 'Embarcadero Delphi XE8';
     17:
-      Result := 'Delphi 10 Seattle';
+      Result := 'Embarcadero Delphi 10 Seattle';
     18:
-      Result := 'Delphi 10.1 Berlin';
+      Result := 'Embarcadero Delphi 10.1 Berlin';
     19:
-      Result := 'Delphi 10.2 Tokyo';
+      Result := 'Embarcadero Delphi 10.2 Tokyo';
     20:
-      Result := 'Delphi 10.3 Rio';
+      Result := 'Embarcadero Delphi 10.3 Rio';
     21:
-      Result := 'Delphi 10.4 Sydney';
+      Result := 'Embarcadero Delphi 10.4 Sydney';
     22:
-      Result := 'Delphi 11 Alexandria';
+      Result := 'Embarcadero Delphi 11 Alexandria';
   End;
 End;
 
@@ -413,7 +446,7 @@ End;
 // TDelphiVersions
 //
 
-Constructor TDelphiVersions.Create;
+Constructor TDelphiVersions.Create(inOwner: TComponent);
 Var
   reg: TRegistry;
   sl: TStringList;
@@ -431,13 +464,16 @@ Var
       reg.CloseKey;
     End;
 
+    sl.Sort;
+
     For s In sl Do
     Begin
       If Not reg.OpenKey(inDelphiVersionClass.BDSRoot + '\' + s, False) Then
         Continue;
 
       Try
-        _versions.Add(inDelphiVersionClass.Create(reg.ReadString('App'), Byte.Parse(s.Substring(0, s.IndexOf('.')))));
+        _latestversion := inDelphiVersionClass.Create(Self, reg.ReadString('App'), Byte.Parse(s.Substring(0, s.IndexOf('.'))));
+        _versions.Add(_latestversion);
       Finally
         reg.CloseKey;
       End;
@@ -447,6 +483,7 @@ Var
 Begin
   inherited;
 
+  _latestversion := nil;
   _versions := TObjectList<TBorlandDelphiVersion>.Create(True);
 
   sl := TStringList.Create;
