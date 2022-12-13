dbc95c1800d8e869c4f54279ce7bdf4973a98b40
AEHimself <57598335+aehimself@users.noreply.github.com>
2022-12-13 12:01:00 +0100

    Created an IDEVersion class so not just Delphi can be managed

---
+++ AE.IDE.DelphiVersions.pas
@@ -0,0 +1,352 @@
+﻿Unit AE.IDE.DelphiVersions;
+
+Interface
+
+Uses System.Generics.Collections, WinApi.Windows, System.SysUtils, System.Classes, WinApi.Messages, AE.DDEManager, AE.IDE.Versions,
+     System.Win.Registry;
+
+Type
+  TDelphiDDEManager = Class(TAEDDEManager)
+  public
+    Constructor Create; ReIntroduce;
+    Procedure OpenFile(Const inFileName: String; Const inPID: Cardinal; Const inTimeOutInMs: Cardinal = 5000);
+  End;
+
+  TDelphiInstance = Class(TIDEInstance)
+  strict protected
+    Procedure InternalFindIDEWindow; Override;
+  public
+    Procedure OpenFile(Const inFileName: String; Const inTimeOutInMs: Cardinal = 5000);
+  End;
+
+  TBorlandDelphiVersion = Class(TIDEVersion)
+  strict protected
+    Procedure InternalRefreshInstances; Override;
+    Function InternalGetName: String; Override;
+    Function InternalNewIDEInstance: Cardinal; Override;
+  public
+    Class Function BDSRoot: String; Virtual;
+  End;
+
+  TDelphiVersionClass = Class Of TBorlandDelphiVersion;
+
+  TBorland2DelphiVersion = Class(TBorlandDelphiVersion)
+  strict protected
+    Function InternalGetName: String; Override;
+  public
+    Class Function BDSRoot: String; Override;
+  End;
+
+  TCodegearDelphiVersion = Class(TBorlandDelphiVersion)
+  strict protected
+    Function InternalGetName: String; Override;
+  public
+    Class Function BDSRoot: String; Override;
+  End;
+
+  TEmbarcaderoDelphiVersion = Class(TBorlandDelphiVersion)
+  strict protected
+    Function InternalGetName: String; Override;
+  public
+    Class Function BDSRoot: String; Override;
+  End;
+
+  TDelphiVersions = Class(TIDEVersions)
+  strict private
+    Procedure DiscoverVersions(Const inRegistry: TRegistry; Const inDelphiVersionClass: TDelphiVersionClass);
+  strict protected
+    Procedure InternalRefreshInstalledVersions; Override;
+  End;
+
+  EDelphiVersionException = Class(Exception);
+
+Implementation
+
+Uses WinApi.PsAPI;
+
+Type
+  TDelphiIDEInfo = Record
+    outHWND: HWND;
+    outWindowCaption: String;
+    PID: Cardinal;
+  End;
+  PDelphiIDEInfo = ^TDelphiIDEInfo;
+
+Function FindDelphiWindow(inHWND: HWND; inParam: LParam): Boolean; StdCall;
+Var
+  ppid: Cardinal;
+  title, classname: Array[0..255] Of Char;
+Begin
+  // https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633498(v=vs.85)
+  // Result := True   ->   Continue evaluation
+  // Result := False  ->   Do not continue evaluation
+
+  GetWindowThreadProcessID(inHWND, ppid);
+  GetWindowText(inHWND, title, 255);
+  GetClassName(inHWND, classname, 255);
+
+  Result := (ppid <> PDelphiIDEInfo(inParam)^.PID) Or Not IsWindowVisible(inHWND) Or (Not IsWindowEnabled(inHWND)) Or Not
+    (String(title).Contains('RAD Studio') Or String(title).Contains('Delphi')) Or (String(classname) <> 'TAppBuilder');
+
+  If Not Result Then
+  Begin
+    PDelphiIDEInfo(inParam)^.outHWND := inHWND;
+    PDelphiIDEInfo(inParam)^.outWindowCaption := title;
+  End;
+End;
+
+//
+// TDelphiDDEManager
+//
+
+Constructor TDelphiDDEManager.Create;
+Begin
+  inherited Create('bds', 'system');
+End;
+
+Procedure TDelphiDDEManager.OpenFile(Const inFileName: String; Const inPID: Cardinal; Const inTimeOutInMs: Cardinal);
+Begin
+  Self.ExecuteCommand('[open("' + inFileName + '")]', inPID, inTimeoutInMs);
+End;
+
+
+//
+// TDelphiInstance
+//
+
+Procedure TDelphiInstance.InternalFindIDEWindow;
+Var
+  info: PDelphiIDEInfo;
+Begin
+  inherited;
+
+  New(info);
+  Try
+    info^.PID := Self.PID;
+    info^.outHWND := 0;
+    info^.outWindowCaption := '';
+
+    EnumWindows(@FindDelphiWindow, LParam(info));
+
+    SetIDEHWND(info^.outHWND);
+    SetIDECaption(info^.outWindowCaption);
+  Finally
+    Dispose(info);
+  End;
+End;
+
+Procedure TDelphiInstance.OpenFile(Const inFileName: String; Const inTimeOutInMs: Cardinal = 5000);
+Var
+  ddemgr: TDelphiDDEManager;
+Begin
+  ddemgr := TDelphiDDEManager.Create;
+  Try
+    ddemgr.OpenFile(inFileName, Self.PID, inTimeOutInMs);
+  Finally
+    FreeAndNil(ddemgr);
+  End;
+End;
+
+//
+// TBorlandDelphiVersion
+//
+
+Class Function TBorlandDelphiVersion.BDSRoot: String;
+Begin
+  Result := 'SOFTWARE\Borland\Delphi';
+End;
+
+Function TBorlandDelphiVersion.InternalNewIDEInstance: Cardinal;
+Var
+  startinfo: TStartupInfo;
+  procinfo: TProcessInformation;
+Begin
+  FillChar(startinfo, SizeOf(TStartupInfo), #0);
+  startinfo.cb := SizeOf(TStartupInfo);
+  FillChar(procinfo, SizeOf(TProcessInformation), #0);
+
+  If Not CreateProcess(PChar(Self.ExecutablePath), nil, nil, nil, False, CREATE_NEW_PROCESS_GROUP, nil, nil, startinfo, procinfo) Then
+    RaiseLastOSError;
+
+  Try
+    WaitForInputIdle(procinfo.hProcess, INFINITE);
+
+    Result := procinfo.dwProcessId;
+  Finally
+    CloseHandle(procinfo.hThread);
+    CloseHandle(procinfo.hProcess);
+  End;
+End;
+
+Procedure TBorlandDelphiVersion.InternalRefreshInstances;
+Var
+  pid: Cardinal;
+  ddemgr: TDelphiDDEManager;
+Begin
+  ddemgr := TDelphiDDEManager.Create;
+  Try
+    For pid In ddemgr.DDEServerPIDs Do
+      If ProcessName(pid).ToLower = Self.ExecutablePath.ToLower Then
+        AddInstance(TDelphiInstance.Create(Self, pid));
+  Finally
+    FreeAndNil(ddemgr);
+  End;
+End;
+
+Function TBorlandDelphiVersion.InternalGetName: String;
+Begin
+  Case Self.VersionNumber Of
+    6:
+      Result := 'Borland Delphi 6';
+    7:
+      Result := 'Borland Delphi 7';
+    Else
+      Result := '';
+  End;
+End;
+
+//
+// TBorland2DelphiVersion
+//
+
+Class function TBorland2DelphiVersion.BDSRoot: String;
+Begin
+  Result := 'SOFTWARE\Borland\BDS';
+End;
+
+Function TBorland2DelphiVersion.InternalGetName: String;
+Begin
+  Case Self.VersionNumber Of
+    3:
+      Result := 'Borland Delphi 2005';
+    4:
+      Result := 'Borland Delphi 2006';
+    5:
+      Result := 'Borland Delphi 2007';
+    Else
+      Result := '';
+  End;
+End;
+
+//
+// TCodegearDelphiVersion
+//
+
+Class Function TCodegearDelphiVersion.BDSRoot: String;
+Begin
+  Result := 'SOFTWARE\CodeGear\BDS';
+End;
+
+Function TCodegearDelphiVersion.InternalGetName: String;
+Begin
+  Case Self.VersionNumber Of
+    6:
+      Result := 'CodeGear Delphi 2009';
+    7:
+      Result := 'CodeGear Delphi 2010';
+    Else
+      Result := '';
+  End;
+End;
+
+//
+// TEmbarcaderoDelphiVersion
+//
+
+Class Function TEmbarcaderoDelphiVersion.BDSRoot: String;
+Begin
+  Result := 'SOFTWARE\Embarcadero\BDS';
+End;
+
+Function TEmbarcaderoDelphiVersion.InternalGetName: String;
+Begin
+  Case Self.VersionNumber Of
+    8:
+      Result := 'Embarcadero Delphi XE';
+    9:
+      Result := 'Embarcadero Delphi XE2';
+    10:
+      Result := 'Embarcadero Delphi XE3';
+    11:
+      Result := 'Embarcadero Delphi XE4';
+    12:
+      Result := 'Embarcadero Delphi XE5';
+    14:
+      Result := 'Embarcadero Delphi XE6';
+    15:
+      Result := 'Embarcadero Delphi XE7';
+    16:
+      Result := 'Embarcadero Delphi XE8';
+    17:
+      Result := 'Embarcadero Delphi 10 Seattle';
+    18:
+      Result := 'Embarcadero Delphi 10.1 Berlin';
+    19:
+      Result := 'Embarcadero Delphi 10.2 Tokyo';
+    20:
+      Result := 'Embarcadero Delphi 10.3 Rio';
+    21:
+      Result := 'Embarcadero Delphi 10.4 Sydney';
+    22:
+      Result := 'Embarcadero Delphi 11 Alexandria';
+  End;
+End;
+
+//
+// TDelphiVersions
+//
+
+Procedure TDelphiVersions.DiscoverVersions(Const inRegistry: TRegistry; Const inDelphiVersionClass: TDelphiVersionClass);
+Var
+  s: String;
+  sl: TStringList;
+Begin
+  sl := TStringList.Create;
+  Try
+    If Not inRegistry.OpenKey(inDelphiVersionClass.BDSRoot, False) Then
+      Exit;
+
+    Try
+      inRegistry.GetKeyNames(sl);
+    Finally
+      inRegistry.CloseKey;
+    End;
+
+    sl.Sort;
+
+    For s In sl Do
+    Begin
+      If Not inRegistry.OpenKey(inDelphiVersionClass.BDSRoot + '\' + s, False) Then
+        Continue;
+
+      Try
+        Self.AddVersion(inDelphiVersionClass.Create(Self, inRegistry.ReadString('App'), Integer.Parse(s.Substring(0, s.IndexOf('.')))));
+      Finally
+        inRegistry.CloseKey;
+      End;
+    End;
+  Finally
+    FreeAndNil(sl);
+  End;
+End;
+
+Procedure TDelphiVersions.InternalRefreshInstalledVersions;
+Var
+  reg: TRegistry;
+Begin
+  inherited;
+
+  reg := TRegistry.Create;
+  Try
+    reg.RootKey := HKEY_CURRENT_USER;
+
+    DiscoverVersions(reg, TBorlandDelphiVersion);
+    DiscoverVersions(reg, TBorland2DelphiVersion);
+    DiscoverVersions(reg, TCodegearDelphiVersion);
+    DiscoverVersions(reg, TEmbarcaderoDelphiVersion);
+  Finally
+    FreeAndNil(reg);
+  End;
+End;
+
+End.
---
+++ AE.IDE.Versions.pas
@@ -0,0 +1,363 @@
+﻿Unit AE.IDE.Versions;
+
+Interface
+
+Uses System.Classes, WinApi.Windows, System.SysUtils, System.Generics.Collections;
+
+Type
+  TIDEInstance = Class(TComponent)
+  strict private
+    _idehwnd: HWND;
+    _idecaption: String;
+    _pid: Cardinal;
+  strict protected
+    Procedure InternalFindIDEWindow; Virtual;
+    Procedure SetIDECaption(Const inIDECaption: String);
+    Procedure SetIDEHWND(Const inIDEHWND: HWND);
+    Function InternalIsIDEBusy: Boolean; Virtual;
+  public
+    Constructor Create(inOwner: TComponent; Const inPID: Cardinal); ReIntroduce; Virtual;
+    Procedure UpdateCaption;
+    Function FindIdeWindow(Const inForceSearch: Boolean = False): Boolean;
+    Function IsIDEBusy: Boolean;
+    Property IDECaption: String Read _idecaption;
+    Property IDEHWND: HWND Read _idehwnd;
+    Property PID: Cardinal Read _pid;
+  End;
+
+  TIDEVersion = Class(TComponent)
+  strict private
+    _executablepath: String;
+    _instances: TObjectList<TIDEInstance>;
+    _name: String;
+    _versionnumber: Integer;
+    Function GetInstances: TArray<TIDEInstance>;
+  strict protected
+    Procedure AddInstance(Const inInstance: TIDEInstance);
+    Procedure InternalRefreshInstances; Virtual;
+    Function InternalGetName: String; Virtual;
+    Function InternalNewIDEInstance: Cardinal; Virtual;
+    Function ProcessName(Const inPID: Cardinal): String;
+  public
+    Constructor Create(inOwner: TComponent; Const inExecutablePath: String; Const inVersionNumber: Integer); ReIntroduce; Virtual;
+    Destructor Destroy; Override;
+    Procedure AfterConstruction; Override;
+    Procedure RefreshInstances;
+    Function InstanceByPID(Const inPID: Cardinal): TIDEInstance;
+    Function IsRunning: Boolean;
+    Function NewIDEInstance: TIDEInstance;
+    Property ExecutablePath: String Read _executablepath;
+    Property Instances: TArray<TIDEInstance> Read GetInstances;
+    Property Name: String Read _name;
+    Property VersionNumber: Integer Read _versionnumber;
+  End;
+
+  TIDEVersions = Class(TComponent)
+  strict private
+    _latestversion: TIDEVersion;
+    _versions: TObjectList<TIDEVersion>;
+    Function GetInstalledVersions: TArray<TIDEVersion>;
+  strict protected
+    Procedure AddVersion(Const inVersion: TIDEVersion);
+    Procedure InternalRefreshInstalledVersions; Virtual;
+  public
+    Constructor Create(inOwner: TComponent); Override;
+    Destructor Destroy; Override;
+    Procedure AfterConstruction; Override;
+    Procedure RefreshInstalledVersions;
+    Function VersionByName(Const inName: String): TIDEVersion;
+    Function VersionByVersionNumber(Const inVersionNumber: Integer): TIDEVersion;
+    Property LatestVersion: TIDEVersion Read _latestversion;
+    Property InstalledVersions: TArray<TIDEVersion> Read GetInstalledVersions;
+  End;
+
+  EAEIDEVersionException = Class(Exception);
+
+Implementation
+
+Uses WinApi.Messages, WinApi.PsAPI;
+
+//
+// TDelphiInstance
+//
+
+Constructor TIDEInstance.Create(inOwner: TComponent; Const inPID: Cardinal);
+Begin
+  inherited Create(inOwner);
+
+  _idehwnd := 0;
+  _idecaption := '';
+  _pid := inPID;
+
+  FindIdeWindow;
+End;
+
+Function TIDEInstance.FindIdeWindow(const inForceSearch: Boolean): Boolean;
+Begin
+  If Not inForceSearch And (_idehwnd <> 0) And IsWindow(_idehwnd) Then
+  Begin
+    // IDE window was already found and seems to be still valid
+
+    Result := True;
+    Exit;
+  End;
+
+  _idehwnd := 0;
+  _idecaption := '';
+
+  Self.InternalFindIDEWindow;
+
+  Result := _idehwnd <> 0;
+End;
+
+Procedure TIDEInstance.InternalFindIDEWindow;
+Begin
+  // Dummy
+End;
+
+Function TIDEInstance.InternalIsIDEBusy: Boolean;
+Var
+  res: NativeInt;
+Begin
+  If Not FindIdeWindow Then
+    Raise EAEIDEVersionException.Create('Delphi IDE window can not be found!');
+
+  Result := Not IsWindowVisible(_idehwnd);
+
+  If Result Then
+    Exit;
+
+  Result := SendMessageTimeout(_idehwnd, WM_NULL, 0, 0, SMTO_BLOCK, 250, nil) = 0;
+
+  If Not Result Then
+    Exit;
+
+  res := GetLastError;
+
+  If res <> ERROR_TIMEOUT Then
+    RaiseLastOSError(res);
+End;
+
+Function TIDEInstance.IsIDEBusy: Boolean;
+Begin
+  Result := Self.InternalIsIDEBusy;
+End;
+
+Procedure TIDEInstance.SetIDECaption(Const inIDECaption: String);
+Begin
+  _idecaption := inIDECaption;
+End;
+
+Procedure TIDEInstance.SetIDEHWND(Const inIDEHWND: HWND);
+Begin
+  _idehwnd := inIDEHWND;
+End;
+
+Procedure TIDEInstance.UpdateCaption;
+Var
+  title: Array[0..255] Of Char;
+Begin
+  If Not FindIdeWindow Then
+    Raise EAEIDEVersionException.Create('Delphi IDE window can not be found!');
+
+  GetWindowText(_idehwnd, title, 255);
+
+  _idecaption := title;
+End;
+
+//
+// TIDEVersion
+//
+
+Procedure TIDEVersion.AddInstance(Const inInstance: TIDEInstance);
+Begin
+  _instances.Add(inInstance);
+End;
+
+Procedure TIDEVersion.AfterConstruction;
+Begin
+  inherited;
+
+  _name := Self.InternalGetName;
+  If _name.IsEmpty Then
+    _name := 'IDE v' + _versionnumber.ToString;
+
+  Self.RefreshInstances;
+End;
+
+Constructor TIDEVersion.Create(inOwner: TComponent; Const inExecutablePath: String; Const inVersionNumber: Integer);
+Begin
+  inherited Create(inOwner);
+
+  _executablepath := inExecutablePath;
+  _instances := TObjectList<TIDEInstance>.Create(True);
+  _name := '';
+  _versionnumber := inVersionNumber;
+End;
+
+Destructor TIDEVersion.Destroy;
+Begin
+  FreeAndNil(_instances);
+
+  inherited;
+End;
+
+Function TIDEVersion.GetInstances: TArray<TIDEInstance>;
+Begin
+  Result := _instances.ToArray;
+End;
+
+Function TIDEVersion.InstanceByPID(Const inPID: Cardinal): TIDEInstance;
+Var
+  inst: TIDEInstance;
+Begin
+  Result := nil;
+
+  For inst In _instances Do
+    If inst.PID = inPID Then
+    Begin
+      Result := inst;
+      Break;
+    End;
+End;
+
+Procedure TIDEVersion.InternalRefreshInstances;
+Begin
+  // Dummy
+End;
+
+Function TIDEVersion.InternalGetName: String;
+Begin
+  // Dummy
+
+  Result := '';
+End;
+
+Function TIDEVersion.InternalNewIDEInstance: Cardinal;
+Begin
+  // Dummy
+
+  Result := 0;
+End;
+
+Function TIDEVersion.IsRunning: Boolean;
+Begin
+  Result := _instances.Count > 0;
+End;
+
+Function TIDEVersion.NewIDEInstance: TIDEInstance;
+Var
+  newpid: Cardinal;
+Begin
+  newpid := Self.InternalNewIDEInstance;
+
+  Result := nil;
+  Repeat
+    Sleep(1000);
+
+    If Not Assigned(Result) Then
+    Begin
+      Self.RefreshInstances;
+
+      Result := Self.InstanceByPID(newpid);
+    End;
+  Until Assigned(Result) And Result.FindIdeWindow And Not Result.IsIDEBusy;
+End;
+
+Function TIDEVersion.ProcessName(Const inPID: Cardinal): String;
+Var
+  processhandle: THandle;
+Begin
+  processhandle := OpenProcess(PROCESS_QUERY_INFORMATION Or PROCESS_VM_READ, False, inPID);
+  If processhandle = 0 Then
+    RaiseLastOSError;
+
+  Try
+    SetLength(Result, MAX_PATH);
+    FillChar(Result[1], Length(Result) * SizeOf(Char), 0);
+    If GetModuleFileNameEx(processhandle, 0, PChar(Result), Length(Result)) = 0 Then
+      RaiseLastOSError;
+
+    Result := Trim(Result);
+  Finally
+    CloseHandle(processhandle)
+  End;
+End;
+
+Procedure TIDEVersion.RefreshInstances;
+Begin
+  _instances.Clear;
+
+  Self.InternalRefreshInstances;
+End;
+
+//
+// TIDEVersions
+//
+
+Procedure TIDEVersions.AddVersion(Const inVersion: TIDEVersion);
+Begin
+  _versions.Add(inVersion);
+
+  If Not Assigned(_latestversion) Or (inVersion.VersionNumber > _latestversion.VersionNumber) Then
+    _latestversion := inVersion;
+End;
+
+Procedure TIDEVersions.AfterConstruction;
+Begin
+  inherited;
+
+  Self.RefreshInstalledVersions;
+End;
+
+Constructor TIDEVersions.Create(inOwner: TComponent);
+Begin
+  inherited;
+
+  _latestversion := nil;
+  _versions := TObjectList<TIDEVersion>.Create(True);
+End;
+
+Destructor TIDEVersions.Destroy;
+Begin
+  FreeAndNil(_versions);
+
+  inherited;
+End;
+
+Function TIDEVersions.GetInstalledVersions: TArray<TIDEVersion>;
+Begin
+  Result := _versions.ToArray;
+End;
+
+Procedure TIDEVersions.InternalRefreshInstalledVersions;
+Begin
+  // Dummy
+End;
+
+Procedure TIDEVersions.RefreshInstalledVersions;
+begin
+  _versions.Clear;
+
+  Self.InternalRefreshInstalledVersions;
+End;
+
+Function TIDEVersions.VersionByName(Const inName: String): TIDEVersion;
+Begin
+  For Result In _versions Do
+    If Result.Name = inName Then
+      Exit;
+
+  Result := nil;
+End;
+
+Function TIDEVersions.VersionByVersionNumber(Const inVersionNumber: Integer): TIDEVersion;
+Begin
+  For Result In _versions Do
+    If Result.VersionNumber = inVersionNumber Then
+      Exit;
+
+  Result := nil;
+End;
+
+End.
