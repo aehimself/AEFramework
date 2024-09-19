5441f864bbf41d89be84de188252d71c1cd8018a
AEHimself <57598335+aehimself@users.noreply.github.com>
2024-09-19 21:48:29 +0200

    Added TAEDLLLoader and TAEDLLAutoLoader

---
+++ AE.DLL.AutoLoader.pas
@@ -0,0 +1,96 @@
+﻿Unit AE.DLL.AutoLoader;
+
+Interface
+
+Uses AE.DLL.Loader;
+
+Type
+  TAEDLLAutoLoader = Class(TAEDLLLoader)
+  strict protected
+    Procedure LoadMethods; Override;
+  End;
+
+Implementation
+
+Uses WinApi.Windows, System.SysUtils;
+
+Type
+  PIMAGE_NT_HEADERS = ^IMAGE_NT_HEADERS;
+  PIMAGE_EXPORT_DIRECTORY = ^IMAGE_EXPORT_DIRECTORY;
+
+Function ImageNtHeader(Base: Pointer): PIMAGE_NT_HEADERS; StdCall; External 'dbghelp.dll';
+Function ImageRvaToVa(NtHeaders: Pointer; Base: Pointer; Rva: ULONG; LastRvaSection: Pointer): Pointer; StdCall; External 'dbghelp.dll';
+
+Procedure TAEDLLAutoLoader.LoadMethods;
+Var
+  a: Integer;
+  filehandle, imagehandle: THandle;
+  imageptr: Pointer;
+  header: PIMAGE_NT_HEADERS;
+  exporttable: PIMAGE_EXPORT_DIRECTORY;
+  namesptr: PCardinal;
+  nameptr: PAnsiChar;
+Begin
+  inherited;
+
+  // https://stackoverflow.com/questions/31917322/how-to-get-all-the-exported-functions-in-a-dll
+
+  filehandle := CreateFile(PChar(Self.DLLName), GENERIC_READ, FILE_SHARE_READ, nil, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+  If filehandle = INVALID_HANDLE_VALUE Then
+    RaiseLastOSError;
+
+  Try
+    imagehandle := CreateFileMapping(filehandle, nil, PAGE_READONLY, 0, 0, nil);
+
+    If imagehandle = 0 Then
+      RaiseLastOSError;
+
+    Try
+      imageptr := MapViewOfFile(imagehandle, FILE_MAP_READ, 0, 0, 0);
+
+      If Not Assigned(imageptr) Then
+        RaiseLastOSError;
+
+      Try
+        header := ImageNtHeader(imageptr);
+
+        If Not Assigned(header) Then
+          RaiseLastOSError;
+
+        If header.Signature <> $00004550 Then // "PE\0\0" as a DWORD.
+          Raise EOSError.Create('Incorrect image NT header signature!');
+
+        exporttable := ImageRvaToVa(header, imageptr, header.OptionalHeader.DataDirectory[0].VirtualAddress, nil);
+
+        If Not Assigned(exporttable) Then
+          RaiseLastOSError;
+
+        namesptr := ImageRvaToVa(header, imageptr, Cardinal(exporttable.AddressOfNames), nil);
+
+        If Not Assigned(namesptr) Then
+          RaiseLastOSError;
+
+        For a := 0 To exporttable.NumberOfNames-1 Do
+        Begin
+          nameptr := ImageRvaToVa(header, imageptr, namesptr^, nil);
+
+          If Not Assigned(nameptr) Then
+            RaiseLastOSError;
+
+          Self.LoadMethod(String(nameptr));
+
+          Inc(namesptr);
+        End;
+      Finally
+        UnmapViewOfFile(imageptr);
+      End;
+    Finally
+      CloseHandle(imagehandle);
+    End;
+  Finally
+    CloseHandle(filehandle);
+  End;
+End;
+
+End.
---
+++ AE.DLL.Loader.pas
@@ -0,0 +1,101 @@
+﻿Unit AE.DLL.Loader;
+
+Interface
+
+Uses Generics.Collections;
+
+Type
+  TAEDLLLoader = Class
+  strict private
+    _dllname: String;
+    _dllhandle: THandle;
+    _methods: TDictionary<String, Pointer>;
+    Function GetMethod(Const inMethodName: String): Pointer;
+    Function GetMethods: TArray<String>;
+  strict protected
+    Procedure LoadMethods; Virtual;
+    Function LoadMethod(Const inMethodName: String): Boolean;
+    Function RaiseExceptionIfUnloadFails: Boolean; Virtual;
+    Property DLLHandle: THandle Read _dllhandle;
+    Property DLLName: String Read _dllname;
+  public
+    Constructor Create(Const inDLLName: String); ReIntroduce; Virtual;
+    Destructor Destroy; Override;
+    Property Method[Const inMethodName: String]: Pointer Read GetMethod; Default;
+    Property Methods: TArray<String> Read GetMethods;
+  End;
+
+Implementation
+
+Uses WinApi.Windows, System.SysUtils;
+
+Constructor TAEDLLLoader.Create(Const inDLLName: String);
+Begin
+  inherited Create;
+
+  _methods := TDictionary<String, Pointer>.Create;
+  _dllname := inDLLName;
+  _dllhandle := 0;
+
+  _dllhandle := LoadLibrary(PChar(_dllname));
+
+  If _dllhandle = 0 Then
+    RaiseLastOSError;
+
+  Self.LoadMethods;
+End;
+
+Destructor TAEDLLLoader.Destroy;
+Begin
+  If _dllhandle <> 0 Then
+  Begin
+    If Not FreeLibrary(_dllhandle) And Self.RaiseExceptionIfUnloadFails Then
+      RaiseLastOSError;
+
+    _dllhandle := 0;
+  End;
+
+  FreeAndNil(_methods);
+
+  inherited;
+End;
+
+Function TAEDLLLoader.GetMethod(Const inMethodName: String): Pointer;
+Begin
+  _methods.TryGetValue(inMethodName, Result);
+End;
+
+Function TAEDLLLoader.GetMethods: TArray<String>;
+Begin
+  Result := _methods.Keys.ToArray;
+
+  TArray.Sort<String>(Result);
+End;
+
+Function TAEDLLLoader.LoadMethod(Const inMethodName: String): Boolean;
+Var
+  tmp: Pointer;
+Begin
+  tmp := getProcAddress(_dllhandle, PChar(inMethodName));
+
+  If Assigned(tmp) Then
+  Begin
+    Result := True;
+
+    _methods.Add(inMethodName, tmp);
+  End
+  Else
+    Result := False;
+End;
+
+Procedure TAEDLLLoader.LoadMethods;
+Begin
+  _methods.Clear;
+End;
+
+Function TAEDLLLoader.RaiseExceptionIfUnloadFails: Boolean;
+Begin
+  Result := True;
+End;
+
+End.
